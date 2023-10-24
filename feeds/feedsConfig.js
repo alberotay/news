6f4eefbaaa96242dd08fbbb0ff2c12479f27b698
alberotay <alberto_acostax@hotmail.com>
2023-10-24 13:31:42 +0200

    cambio orden feeds

--- feeds/feedsConfig.js
+++ feeds/feedsConfig.js
@@ -1,7 +1,7 @@
 let categories = require('./feedCategories').CATEGORIES
 
 let feedsConfig = [
-    ["newYorkTimes", 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml', categories.INTERNATIONAL],
+    ["elMundo","https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml",categories.NATIONAL],
     ["laVanguardia", "https://www.lavanguardia.com/rss/home.xml", categories.NATIONAL],
     ["elAbc", "https://www.abc.es/rss/2.0/portada/", categories.NATIONAL],
     ["20Minutos", "https://www.20minutos.es/rss/", categories.CATALONIA],
@@ -18,11 +18,11 @@ let feedsConfig = [
     ["sport", "https://www.sport.es/es/rss/last-news/news.xml", categories.SPORTS],
     ["marca", "https://e00-marca.uecdn.es/rss/portada.xml", categories.SPORTS],
     ["bbc", 'https://www.bbc.com/mundo/ultimas_noticias/index.xml', categories.INTERNATIONAL],
+    ["newYorkTimes", 'https://rss.nytimes.com/services/xml/rss/nyt/World.xml', categories.INTERNATIONAL],
     ["okDiario", "https://okdiario.com/feed", categories.NATIONAL],
     ["as", "https://as.com/rss/tags/ultimas_noticias.xml", categories.SPORTS],
     ["washingtonpPost","https://feeds.washingtonpost.com/rss/national?itid=lk_inline_manual_32",categories.INTERNATIONAL],
     ["laTribuna","https://www.latribunadeciudadreal.es/RSS/TCRPortada.xml",categories.CASTILLALAMANCHA],
-    ["elMundo","https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml",categories.NATIONAL],
     ["miCiudadReal","https://www.miciudadreal.es/feed/",categories.CASTILLALAMANCHA],
     ["enCastilla","https://www.encastillalamancha.es/feed/",categories.CASTILLALAMANCHA]
 ]
